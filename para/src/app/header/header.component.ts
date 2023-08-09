99d86a9444f4c93c6db35960d6304f76b549dbd7
marwa guerfel <marwaguerfel76@gmail.com>
2023-08-09 13:25:34 +0100

    lll

--- para/src/app/header/header.component.ts
+++ para/src/app/header/header.component.ts
@@ -1,36 +1,22 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
 import { Category } from '../models/category';
 import { CategoryService } from '../services/category.service';
-=======
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import { LoginService } from '../services/login.service';
->>>>>>> b57257c6aac3fdef2aedb1eb681094996e2bd2f8
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-<<<<<<< HEAD
-export class HeaderComponent implements OnInit{
-=======
+
 export class HeaderComponent implements OnInit {
     username !:string;
     isLogedIn=false;
 
->>>>>>> b57257c6aac3fdef2aedb1eb681094996e2bd2f8
     isOpen = false;
-  constructor(private LoginService: LoginService) {}
+  constructor(private LoginService: LoginService,private categoryService: CategoryService) {}
 
-  ngOnInit() {
-    if(localStorage.getItem('token')!=null){
-      this.isLogedIn=true;
-      // @ts-ignore
-      this.username=localStorage.getItem('username');
-    };
-  }
 
  async logout() {
   await  this.LoginService.logout();
@@ -40,9 +26,13 @@ export class HeaderComponent implements OnInit {
       this.isOpen = !this.isOpen;
     }
     categories: Category[] = [];
-    constructor(private categoryService: CategoryService) {}
 
     ngOnInit() {
+      if(localStorage.getItem('token')!=null){
+        this.isLogedIn=true;
+        // @ts-ignore
+        this.username=localStorage.getItem('username');
+      };
       this.getCategories().subscribe(categories => {
         this.categories = categories;
       });
